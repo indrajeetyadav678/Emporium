6af6321c796d3547688717ca8614bcb98778ea28
sumit umariya <sumitumariya11@gmail.com>
2024-07-03 13:06:45 +0530

    created error page

--- front_end/frontend_emporium/src/App.jsx
+++ front_end/frontend_emporium/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import About from './about'
+import Error from './Error'
 
 function App() {
   return (
     <>
-      <About/>
+      {/* <About/> */}
+      <Error/>
      
       
     </>
