3357558a0fb3e57e5787d2634a70a9d599c0e0df
khushi <sourabhpatel63679@gmail.com>
2024-07-10 16:38:37 +0530

    working_on_home_page

--- front_end/frontend_emporium/src/component_cutomer_02/Home.jsx
+++ front_end/frontend_emporium/src/component_cutomer_02/Home.jsx
@@ -1,4 +1,4 @@
-
+import "./style/home.css";
 
 
 
@@ -6,7 +6,23 @@ const Home=()=>{
     return(
 
          <>
-         <h1>Welcome to Home Page</h1>
+         <div className="home_container">
+            
+            <div className="banner_slide">
+                <div className="slider">
+                </div>
+            </div>
+            <div id="offer-prize">
+                <div id="box1"> <h6 style={{color:"white"}}> Toys from 399 </h6> </div>
+                <div id="box2" > <h6 align="bottom" style={{color:"white"}}> Kids from 499 </h6> </div>
+                <div id="box3"> <h6 style={{color:"white"}}> Men's from 599 </h6>  </div>
+                <div id="box4"> <h6 style={{color:"white"}}> Woman's 699 </h6> </div>
+                <div id="box5"> <h6 style={{color:"white"}}> Woman's 699 </h6> </div>
+
+            </div>
+
+
+         </div>
         </>
     )
         
