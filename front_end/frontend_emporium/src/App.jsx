70cc3c54a14cad5bb9529050e87bf3afa0ec823c
amisha <amishamalviya2607@gmail.com>
2024-07-11 16:59:54 +0530

    Started Working on Contact_JSX

--- front_end/frontend_emporium/src/App.jsx
+++ front_end/frontend_emporium/src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 
-import About from './about';
-import Error from './Error';
-import Home from "./Home";
-import Layout from "./Layout";
+// import About from './about';
+// import Error from './Error';
+// import Home from "./Home";
+// import Layout from "./Layout";
 // import Contact from "./Contact";
 // import Signup from "./Signup";
-import Checkout from "./checkout";
+// import Checkout from "./checkout";
 // import { useState } from 'react'
 import About from './component_cutomer_02/about';
 import Home from "./component_cutomer_02/Home";
 import Layout from "./Layout";
 import SignUp from "./component_customer_01/SignUp";
 import SignIn from "./component_customer_01/SignIn";
+import Contact from "./component_cutomer_02/Contact";
 
 import Checkout from "./checkout";
 import Cart from "./component_cutomer_02/Cart";
@@ -29,7 +30,7 @@ const App=()=>{
           <Route index element={<Home/>} />
           
             <Route path="home" element={<Home/>} />
-            {/* <Route path="contact" element={<Contact/>} /> */}
+            <Route path="contact" element={<Contact/>} />
             <Route path="about" element={<About/>} />
             {/* <Route path="sign_up" element={<Signup/>} /> */}
             <Route path="checkout" element={<Checkout/>} />
