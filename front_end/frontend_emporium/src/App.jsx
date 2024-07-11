99236bf316bb4b557e60c3d6cbb92b86916bc401
khushi <sourabhpatel63679@gmail.com>
2024-07-11 17:07:46 +0530

    Updated_Landing_page

--- front_end/frontend_emporium/src/App.jsx
+++ front_end/frontend_emporium/src/App.jsx
@@ -1,12 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 
-import About from './about';
-import Error from './Error';
-import Home from "./Home";
-import Layout from "./Layout";
-// import Contact from "./Contact";
-// import Signup from "./Signup";
+
 import Checkout from "./checkout";
 // import { useState } from 'react'
 import About from './component_cutomer_02/about';
@@ -15,7 +10,7 @@ import Layout from "./Layout";
 import SignUp from "./component_customer_01/SignUp";
 import SignIn from "./component_customer_01/SignIn";
 
-import Checkout from "./checkout";
+// import Checkout from "./checkout";
 import Cart from "./component_cutomer_02/Cart";
 
 
