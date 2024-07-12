7676e44be596a1586236bc32aeb8ef84b3892315
khushi <sourabhpatel63679@gmail.com>
2024-07-12 16:55:51 +0530

    created_homeTheater_page

--- front_end/frontend_emporium/src/App.jsx
+++ front_end/frontend_emporium/src/App.jsx
@@ -1,32 +1,46 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
 
+
+import Checkout from "./checkout";
+// import { useState } from 'react'
 import About from './component_cutomer_02/about';
 import Home from "./component_cutomer_02/Home";
 import Layout from "./Layout";
 import SignUp from "./component_customer_01/SignUp";
 import SignIn from "./component_customer_01/SignIn";
-import Contact from "./component_cutomer_02/Contact";
 
-import Checkout from "./checkout";
+// import Checkout from "./checkout";
 import Cart from "./component_cutomer_02/Cart";
-
+import HomeTheater from "./component_cutomer_02/home_theater_page";
 
 const App=()=>{
   return(
-    <>    
+    <>
+     
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>} />      
+          <Route index element={<Home/>} />
+          
             <Route path="home" element={<Home/>} />
-            <Route path="contact" element={<Contact/>} />
+            {/* <Route path="contact" element={<Contact/>} /> */}
             <Route path="about" element={<About/>} />
+            {/* <Route path="sign_up" element={<Signup/>} /> */}
             <Route path="checkout" element={<Checkout/>} />
             <Route path="about" element={<About/>} />
+
             <Route path="sign_up" element={<SignUp/>} />
+
             <Route path="signin" element={<SignIn/>} />
+
+
+
             <Route path="cart" element={<Cart/>} />
-            <Route path="checkout" element={<Checkout/>} />          
+            <Route path="checkout" element={<Checkout/>} />
+            <Route path="home_theater_page" element={<HomeTheater/>}/>
+
+           
           </Route>
         </Routes>
       </BrowserRouter>
