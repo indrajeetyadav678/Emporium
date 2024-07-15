bbffac63c75ecc52886319e01e8deb514e22d23d
Indrajeet <yindrajeet678@gmail.com>
2024-07-15 23:40:09 +0530

    additions

--- front_end/frontend_emporium/src/addproducts.jsx
+++ front_end/frontend_emporium/src/addproducts.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import axios from "axios"
+
 import "./addproducts.css";
 
+
 const Addproduct = () => {
+     const [product, SetProducts] = useState({})
+     
+
 
     return(
         <>
@@ -9,21 +16,21 @@ const Addproduct = () => {
         <br/> <br/> <br/> <br/> 
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Name</label>
-          <input type="text" class="form-control" name="name" aria-describedby="emailHelp" />
+          <input type="text" class="form-control" name="product_name" value={product.product_name} onChange={inputhandle} aria-describedby="emailHelp" />
         </div> <br/> <br/>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Price</label>
-          <input type="tel" class="form-control" name="price" aria-describedby="emailHelp" />
+          <input type="tel" class="form-control" name="price" value={product.price} onChange={inputhandle} aria-describedby="emailHelp" />
         </div> <br/> <br/>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Description</label>
-          <input type="text" class="form-control" name="img" aria-describedby="emailHelp" />
+          <input type="text" class="form-control" name="img" value={product.name} onChange={inputhandle} aria-describedby="emailHelp" />
         </div> <br/> <br/>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Image</label>
-          <input type="file" class="form-control" name="img" aria-describedby="emailHelp" />
+          <input type="file" class="form-control" name="img" value={product.name} onChange={inputhandle} aria-describedby="emailHelp" />
         </div> <br/> <br/>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" onClick={submithandle}>Submit</button>
       </form>
       </center>
       <br /> <br />
