083a39fb371b4c6a8e06e5067d1732c64b8d1b7f
Indrajeet <yindrajeet678@gmail.com>
2024-07-29 22:43:59 +0530

    minor change in layout

--- front_end/frontend_emporium/src/Layout.jsx
+++ front_end/frontend_emporium/src/Layout.jsx
@@ -9,7 +9,7 @@ import { AiOutlineBars } from "react-icons/ai";
 
 import Footer from "./component_customer_011/Footer";
 import { useNavigate } from "react-router-dom";
-import {useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./index.css";
 
 
@@ -24,53 +24,53 @@ const Layout = () => {
     }
     return (
         <>
-         <input type="checkbox" id="navid"/>
-        
+            <input type="checkbox" id="navid" />
+
             <header className="header1">
-                
-                        <div className="head-sub1">
-                    <marquee  scrollamount="10" behavior="alternate">
-                            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! ShopNow
-                        </marquee>
-                        </div>
-                    <div className="head-sub2">
-                        <select>
-                            <option>English</option>
-                            <option>Spanish</option>
-                            <option>French</option>
-                            <option>Japanese</option>
-                            <option>Russian</option>
-                            <option>Hindi</option>
-                        </select>
-                    </div>
+
+                <div className="head-sub1">
+                    <marquee scrollamount="10" behavior="alternate">
+                        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! ShopNow
+                    </marquee>
+                </div>
+                <div className="head-sub2">
+                    <select>
+                        <option>English</option>
+                        <option>Spanish</option>
+                        <option>French</option>
+                        <option>Japanese</option>
+                        <option>Russian</option>
+                        <option>Hindi</option>
+                    </select>
+                </div>
             </header>
             <div className="nav-container">
-                        <span>Electronic Emporium</span>
-                        <ul >
-                            <li> <Link to="home" className="head-ul">Home</Link> </li>
-                            <li> <Link to="contact"className="head-ul" >Contact</Link> </li>
-                            <li> <Link to="about" className="head-ul">About</Link> </li>
-                            {/* {user? */}
-                            (<li> <Link to="sign_up"className="head-ul" >Sign-up</Link> </li>):
-                            (<li> <Link to="sign_up" className="head-ul">Logout</Link> </li>)
-                            {/* } */}
-                            {/* <li> <Link to="addproduct" className="head-ul">addproduct</Link> </li> */}
+                <span>Electronic Emporium</span>
+                <ul >
+                    <li> <Link to="home" className="head-ul">Home</Link> </li>
+                    <li> <Link to="contact" className="head-ul" >Contact</Link> </li>
+                    <li> <Link to="about" className="head-ul">About</Link> </li>
+                    {/* {user? */}
+                    (<li> <Link to="sign_up" className="head-ul" >Sign-up</Link> </li>):
+                    (<li> <Link to="sign_up" className="head-ul">Logout</Link> </li>)
+                    {/* } */}
+                    <li> <Link to="addproduct" className="head-ul">addproduct</Link> </li>
 
-                        </ul>
-                        
-                        <div className="search-bar">
-                            <input type="text" placeholder="What are you looking for?" />
-                               <IoIosSearch  className="search-icon"/>
-                        </div>
-                        <div className="head-icon">
-                            <li className="head-ul iconh"><IoHeartOutline /></li>
-                            <li className="head-ul iconh" onClick={Incart}><BsCart3 /></li>
-                            <li className="head-ul iconh"><FiUser /></li>
+                </ul>
 
-                        </div>
-                        <label for ="navid" className="label"><AiOutlineBars /></label>
+                <div className="w-[450px] flex justify-between items-center "> 
+                    <div>
+                        <input type="text" placeholder="What are you looking for?" />
+                        <IoIosSearch className="search-icon" />
+                    </div>
+                    <div className="head-ul iconh"><IoHeartOutline /></div>
+                    <div className="head-ul iconh" onClick={Incart}><BsCart3 /></div>
+                    <div className="head-ul iconh"><FiUser /></div>
+
+                </div>
+                    <label for="navid" className="label"><AiOutlineBars /></label>
             </div>
-            
+
             <Outlet />
             <Footer />
         </>
